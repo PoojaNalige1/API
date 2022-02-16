5894191cc11173e660d3694b8d497436fbb50ea9
PoojaNalige1 <99814328+PoojaNalige1@users.noreply.github.com>
2022-02-16 20:03:44 +0530

    Add files via upload

---
+++ CustomerMgmtSys/Migrations/20220212060846_a2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CustomerMgmtSys.Migrations
+{
+    public partial class a2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Customers",
+                newName: "CustomerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CustomerId",
+                table: "Customers",
+                newName: "Id");
+        }
+    }
+}
---
+++ CustomerMgmtSys/Models/EFDBhandle.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerMgmtSys.Models
+{
+    public class EFDBhandle:DbContext
+    {
+        public string conn = "Server=DESKTOP-MSP08OQ;Database= CustomerMgmt_DB; Trusted_Connection=True;";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsB)
+        {
+            optionsB.UseSqlServer(conn);
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
